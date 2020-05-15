ef1bc81f1b56809a1aca397f59283f3f42303bfb
ratroot92 <zcomisb.007@gmail.com>
2020-05-16 02:06:06 +0500

    friday night

--- core/models.py
+++ core/models.py
@@ -4,9 +4,33 @@ from django.conf import settings
 # Create your models here.
 
 
+CATEGORY_CHIOCES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport Wear'),
+    ('OW', 'Outwear'),
+
+
+)
+
+
+LABEL_CHIOCES = (
+    ('pri', 'primary'),
+    ('sec', 'secondary'),
+    ('inf', 'info'),
+    ('dan', 'danger'),
+    ('dar', 'dark'),
+    ('suc', 'successs'),
+    ('war', 'warning'),
+
+
+)
+
+
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHIOCES, max_length=2)
+    label = models.CharField(choices=LABEL_CHIOCES, max_length=3)
 
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Item, Order, OrderItem
+from django.views.generic import TemplateView, View, DetailView, ListView
+
 # Create your views here.
 
 
-def item_list(request):
-    items = Item.objects.all()
-    return render(request, 'home-page.html', {'items': items})
+class Home_View(ListView):
+    model = Item
+    template_name = "home-page.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'items': Item.objects.all()
+        }
+
+    return render(request, 'home-page.html', context)
